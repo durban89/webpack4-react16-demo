e6973c5d354cfc3331881283633e6d243aacbd76
durban.zhang <durban.zhang@gmail.com>
2018-06-06 14:23:25 +0800

    添加react开发相关的逻辑

--- webpack.config.js
+++ webpack.config.js
@@ -5,27 +5,39 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    app: ['./src/index.js'],
-    print: ['./src/print.js'],
+    app: ['./src/index.jsx'],
   },
   devtool: 'eval',
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 8083,
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Webpack-输出管理'
+      title: 'React Demo',
+      filename: './index.html', // 调用的文件
+      template: './index.html', // 模板文件
     }),
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: [
+          path.resolve(__dirname, 'node_modules'),
+        ],
+        options: {
+          plugins: ['transform-async-to-generator', 'transform-strict-mode', 'transform-object-assign', 'transform-decorators-legacy'],
+          presets: ['es2015', 'react', 'stage-0'],
+        },
+      },
       {
         test: /\.css$/,
         use: [
@@ -37,26 +49,29 @@ module.exports = {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           'file-loader',
-        ]
+        ],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           'file-loader',
-        ]
+        ],
       },
       {
         test: /\.(csv|tsv)$/,
         use: [
           'csv-loader',
-        ]
+        ],
       },
       {
         test: /\.xml$/,
         use: [
           'xml-loader',
-        ]
-      }
+        ],
+      },
     ],
-  }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // 这里是必须要加的，不然默认的值加载['.js','.json']为后缀的文件
+  },
 };
